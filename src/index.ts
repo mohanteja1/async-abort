f9e4cfd50811d3873172c23c8d6f42b98cd4e733
mohan <mohan.venkata@anywhere.co>
2022-03-06 23:12:21 +0530

    fix: cancel func holding refs in closure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 type Callback = (...args: any[]) => any;
 type AsyncFunction = (...args: any[]) => Promise<any>;
+type BlockType = 'then' | 'catch' | 'finally';
+type Block = { type: BlockType, cb: Callback };
 
 class Observer {
     cbs: Map<string, Callback>;
@@ -9,8 +11,9 @@ class Observer {
         this.count = 0;
     }
 
-    add(id: string, cb: Callback) {
-      if(!this.cbs.has(id)) this.cbs.set(id, cb);
+    add(id: string, cbs: Array<Block>) {
+      const combined = ({ index, value } : { value: any, index: number }) => cbs[index].cb(value);
+      if(!this.cbs.has(id)) this.cbs.set(id, combined);
     }
 
     remove(id: string) {
@@ -20,56 +23,67 @@ class Observer {
     emit(id: string, event: any) {
       return this.cbs.get(id)?.(event);
     }
+
+    createCancel(id: string) {
+      return () => this.remove(id);
+    }
 }
 
 const PromiseObserver = new Observer();
+
+function extendPromise(id: string, promise: Promise<any>, chainTypes: Array<BlockType>) {
+  chainTypes.forEach((type , index) => {
+    switch (type) {
+      case 'then':
+        promise = promise.then((value) => PromiseObserver.emit(id, { index, value }));
+        break;
+      case 'catch':
+        promise = promise.catch((error) => PromiseObserver.emit(id, { index, value: error }));
+        break;
+      case 'finally':
+        promise = promise.finally(() => PromiseObserver.emit(id, { index }));
+        break;
+    }
+  });
+}
+
 class AsyncAbort {
   private id: string;
   private func: AsyncFunction;
   private args: Array<any>;
-  private chain: Array<{ type: 'then' | 'catch' | 'finally', cb: Callback }>;
+  private chains: Array<Block>;
 
   constructor(func: AsyncFunction, args: Array<any> = []) {
     this.id = `${Date.now()}_${++PromiseObserver.count}`;
     this.func = func;
     this.args = args;
-    this.chain = [];
+    this.chains = [];
   }
 
   then(cb: Callback): AsyncAbort {
-    this.chain.push({ type: 'then', cb});
+    this.chains.push({ type: 'then', cb});
     return this;
   }
 
   catch(cb: Callback): AsyncAbort {
-    this.chain.push({ type: 'catch', cb });
+    this.chains.push({ type: 'catch', cb });
     return this;
   }
 
   finally(cb: Callback): AsyncAbort {
-    this.chain.push({ type: 'finally', cb });
+    this.chains.push({ type: 'finally', cb });
     return this;
   }
 
   call(): Callback {
-    this.chain.push({ type: 'finally', cb: () => PromiseObserver.remove(this.id)});
-    PromiseObserver.add(this.id, ({ index, value } : { value: any, index: number }) => this.chain[index].cb(value));
     const id = this.id;
-    let promise = this.func(...this.args);
-    this.chain.forEach(({ type }, index) => {
-      switch (type) {
-        case 'then':
-          promise = promise.then((value) => PromiseObserver.emit(this.id, { index, value }));
-          break;
-        case 'catch':
-          promise = promise.catch((error) => PromiseObserver.emit(this.id, { index, value: error }));
-          break;
-        case 'finally':
-          promise = promise.finally(() => PromiseObserver.emit(this.id, { index }));
-          break;
-      }
-    });
-    return () => PromiseObserver.remove(id);
+    this.chains.push({ type: 'finally', cb: PromiseObserver.createCancel(id)});
+    PromiseObserver.add(id, [...this.chains]);
+    const types = this.chains.map(({ type }) => type);
+    const promise = this.func(...this.args);
+    extendPromise(id, promise, types);
+    this.chains = this.args = this.func = this.id = undefined;
+    return PromiseObserver.createCancel(id);
   }
 }
 
